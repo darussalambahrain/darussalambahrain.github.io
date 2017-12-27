c778c9019d3b5a0c80803b25f629d78907112281
mdmsb <msb.arkon@gmail.com>
2017-12-27 15:09:40 +0530

    d

---
+++ myscript.js
@@ -0,0 +1,10 @@
+function myFunction() {
+   document.getElementById("headerspawn").innerHTML = "<img class='logo' src='img/tlogo.png'><img class='xs-hidden logo2' src='img/translogo2.png'><div class='contactheader xs-hidden'><span>Contact Us</span><br><span class='contactnumber'>17330786</span></div><div class='menuholder xs-visible'><div class='menu' id='menubars'><div class='linebar'></div><div class='linebar'></div><div class='linebar'></div></div></div>";
+
+   document.getElementById("menutoggle").innerHTML = "<ul><li><a id='index' href='index.html'>Home</a></li><li><a id='about' href='about-us.html'>About-us</a></li><li><a id='services' href='services.html'>Services</a></li><li><a id='gallery' href='gallery.html'>Gallery</a></li><li><a id='contact' href='contact-us.html'>Contact</a></li></ul>";
+
+   document.getElementById("footersection").innerHTML = "<div class='col-sm-3 xs-hidden'><img src='img/f.jpg' class='sm' href=''><img src='img/t.jpg' class='sm' href=''><img src='img/g.jpg' class='sm' href=''><img src='img/in.jpg' class='sm' href=''></div><div class='col-sm-6' style=''><br>	<img class='logofooter' src='img/tlogo.png'> <img class='logofooter2' src='img/translogo2.png'></div><div class='col-sm-3' style='color:#091834;'>Flat:, Building:, Road:,<br>Block:, Al Seef.<br>Kingdom of Bahrain, <br>Mobile:00973-00000000<br>Email: info@darussalambaharain.com</div>	<div class='col-sm-3 xs-visible'>	<img src='img/f.jpg' class='sm' href=''>	<img src='img/t.jpg' class='sm' href=''>	<img src='img/g.jpg' class='sm' href=''>	<img src='img/in.jpg' class='sm' href=''>	</div>";
+
+   document.getElementById("footer").innerHTML = "<ul><li>© Copyright&nbsp;&nbsp;|&nbsp;&nbsp;</li><li>Privacy Policy&nbsp;&nbsp;|&nbsp;&nbsp;</li><li><a style='color:#b3afaf;'>Web Design by musab</a></li></ul>";
+   
+}
